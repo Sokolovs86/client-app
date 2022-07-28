06886996063ce8a878ff0263597393e4870b28eb
Andrei Sokolov <sokolovsandrey@gmail.com>
2022-07-28 13:19:13 +0500

    add 28076

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './chat/Home';
+import Contact from './chat/Contact';
+import Message from './chat/Message';
 
 function App() {
   return (
-    
+    <Router>
+      <Route path="/" component={Home}></Route>
+      <Route path="message" component={Message}></Route>
+      <Route path="contact" component={Contact}></Route>
+    </Router>
   );
 }
 
