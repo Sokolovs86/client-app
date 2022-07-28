a685b208a047f3f8ba9c34830c053e107cac1333
Andrei Sokolov <sokolovsandrey@gmail.com>
2022-07-28 12:56:48 +0500

    add 28-07-22-3

---
+++ src/chat/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return <div className="container">
+        <div className="row">
+            <h1 className="text-center">Home Page</h1>
+        </div>
+    </div>
+}
+export default Home;
